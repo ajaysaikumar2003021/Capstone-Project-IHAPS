e6554d4993585956a2a04e1824d08a06a126a35c
Devil <bhuttisss@gmail.com>
2022-04-24 22:52:17 +0500

    asdfasdf

--- src/serverurl.js
+++ src/serverurl.js
@@ -1,3 +1,4 @@
 // export const URL_SERVER = 'http://184.73.81.44:8080';
 // export const URL_SERVER = 'http://localhost:8000';
-export const URL_SERVER = 'https://backend-ihaps.herokuapp.com'
+// export const URL_SERVER = 'https://backend-ihaps.herokuapp.com'
+export const URL_SERVER = 'http://backend-ihaps.herokuapp.com'
